a9f487eb724b4ed57d7ee98a89e2a013054e7004
yasminenassar <ynassar@ucsd.edu>
2019-04-28 18:34:36 -0700

    added some files

---
+++ static_files/js/functionality.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  console.log("are u ready");
+})
+
+
+function login() {
+  var user = document.getElementById("user").value;
+  var pass = document.getElementById("pass").value;
+  location.replace("index.html");
+}
+
+function createAccount() {
+  console.log("creating an account");
+  //var user = document.getElementById("user").value;
+  //var pass = document.getElementById("pass").value;
+  location.replace("profile.html");
+
+}
